587ef1c15ca2745f3cfe24c2dddbddcf04b0f369
JP Cimalando <jpcima@users.noreply.github.com>
2018-06-04 14:16:29 +0200

    XG parameter changes

--- lisp/xg.scm
+++ lisp/xg.scm
@@ -3,8 +3,13 @@
 ;;    (See accompanying file LICENSE or copy at
 ;;          http://www.boost.org/LICENSE_1_0.txt)
 
+(use-modules (srfi srfi-43))
+
 (export
- xg:system-on)
+ xg:system-on
+ xg:parameter-change
+ xg:parameter-change/u7
+ xg:parameter-change/u14)
 
 (define* (xg:system-on #:key (id *device-identifier*))
   (ms:write (vector #xf0 #x43
@@ -13,3 +18,27 @@
                     #x00 #x00 #x7e
                     #x00
                     #xf7)))
+
+(define* (xg:parameter-change address data #:key (id *device-identifier*) (model #x4c))
+  (ms:write
+   (vector-append
+    (vector #xf0 #x43
+            id
+            model
+            (ensure-range (ash address -16) 0 127 "address[0]")
+            (ensure-range (logand (ash address -8) #xff) 0 127 "address[1]")
+            (ensure-range (logand address #xff) 0 127 "address[2]"))
+    data
+    (vector #xf7))))
+
+(define* (xg:parameter-change/u7 address value #:key (id *device-identifier*) (model #x4c))
+  (xg:parameter-change address
+                       (vector (ensure-range value 0 127 "value"))
+                       #:id id #:model model))
+
+(define* (xg:parameter-change/u14 address value #:key (id *device-identifier*) (model #x4c))
+  (xg:parameter-change address
+                       (vector
+                        (ensure-range (ash value -7) 0 127 "value")
+                        (logand value #x7f))
+                       #:id id #:model model))
