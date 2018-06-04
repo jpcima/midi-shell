ee4020d89b7be96e0c0651d8e15daa2ef13ccb59
JP Cimalando <jpcima@users.noreply.github.com>
2018-06-04 11:13:59 +0200

    initial commit

---
+++ lisp/stdlib.scm
@@ -0,0 +1,100 @@
+;;          Copyright Jean Pierre Cimalando 2018.
+;; Distributed under the Boost Software License, Version 1.0.
+;;    (See accompanying file LICENSE or copy at
+;;          http://www.boost.org/LICENSE_1_0.txt)
+
+(export
+ ms:write
+ ms:channel
+ ms:velocity
+ ms:set-channel!
+ ms:set-velocity!
+ ms:noteon
+ ms:noteoff
+ ms:control
+ ms:pitch-bend
+ ms:program
+ ms:rpn
+ ms:nrpn)
+
+(define *channel* 0)
+(define *velocity* 100)
+
+(define (ensure-range val min max name)
+  (unless (<= min val max)
+    (error 'out-of-range "`~a` is expected to be in the range ~a..~a" name min max))
+  val)
+
+(define (ms:channel)
+  channel)
+
+(define (ms:velocity)
+  velocity)
+
+(define (ms:set-channel! channel)
+  (ensure-range channel 0 15 "channel")
+  (set! *channel* channel))
+
+(define (ms:set-velocity! velocity)
+  (ensure-range velocity 0 127 "velocity")
+  (set! *velocity* velocity))
+
+(define* (ms:noteon note #:key (channel *channel*) (velocity *velocity*))
+  (ensure-range note 0 127 "note")
+  (ensure-range channel 0 15 "channel")
+  (ensure-range velocity 1 127 "velocity")
+  (ms:write (vector (+ #x90 channel) note velocity)))
+
+(define* (ms:noteoff note #:key (channel *channel*) (velocity *velocity*))
+  (ensure-range note 0 127 "note")
+  (ensure-range channel 0 15 "channel")
+  (ensure-range velocity 0 127 "velocity")
+  (ms:write (vector (+ #x80 channel) note velocity)))
+
+(define* (ms:control ctrl value #:key (channel *channel*))
+  (ensure-range ctrl 0 127 "control")
+  (ensure-range channel 0 15 "channel")
+  (ensure-range value 0 127 "value")
+  (ms:write (vector (+ #xb0 channel) ctrl value)))
+
+(define* (ms:pitch-bend value #:key (channel *channel*))
+  (ensure-range value -8192 8191 "bend")
+  (let* ((value (+ 8192 value))
+         (lsb (modulo value 128))
+         (msb (floor (/ value 128))))
+    (ms:write (vector (+ #xe0 channel) lsb msb))))
+
+(define* (ms:program pgm #:key (channel *channel*))
+  (ensure-range pgm 0 127 "program")
+  (ensure-range channel 0 15 "channel")
+  (ms:write (vector (+ #xc0 channel) pgm)))
+
+(define (combine-msb-lsb pair)
+  (+ (ensure-range (cdr pair) 0 127 "lsb")
+     (* 128 (ensure-range (car pair) 0 127 "msb"))))
+
+(define* (ms:rpn rpn value #:key (channel *channel*))
+  (cond
+   ((pair? rpn) (ms:rpn (combine-msb-lsb rpn) value))
+   ((pair? value) (ms:rpn rpn (combine-msb-lsb value)))
+   (#t (ensure-range rpn 0 16383 "rpn")
+       (ensure-range value 0 16383 "value")
+       (ms:control 101 (floor (/ rpn 128)) #:channel channel)
+       (ms:control 100 (modulo rpn 128) #:channel channel)
+       (ms:control 6 (floor (/ value 128)) #:channel channel)
+       (ms:control 38 (modulo value 128) #:channel channel)
+       (ms:control 101 127 #:channel channel)
+       (ms:control 100 127 #:channel channel))))
+
+(define* (ms:nrpn rpn value #:key (channel *channel*))
+  (cond
+   ((pair? rpn) (ms:nrpn (combine-msb-lsb rpn) value))
+   ((pair? value) (ms:nrpn rpn (combine-msb-lsb value)))
+   (#t (ensure-range rpn 0 16383 "rpn")
+       (ensure-range value 0 16383 "value")
+       (ms:control 99 (floor (/ rpn 128)) #:channel channel)
+       (ms:control 98 (modulo rpn 128) #:channel channel)
+       (ms:control 6 (floor (/ value 128)) #:channel channel)
+       (ms:control 38 (modulo value 128) #:channel channel)
+       (ms:control 99 127 #:channel channel)
+       (ms:control 98 127 #:channel channel))))
---
+++ sources/midi-shell.cc
@@ -0,0 +1,111 @@
+//          Copyright Jean Pierre Cimalando 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <RtMidi.h>
+#include <libguile.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <unistd.h>
+
+static std::vector<const char *> args_to_argv(
+    const std::vector<std::string> &args);
+
+static RtMidiOut *ms_port = nullptr;
+static std::vector<uint8_t> ms_writebuf;
+static SCM ms_write(SCM msg);
+
+///
+int main(int argc, char *argv[])
+{
+  std::vector<std::string> scm_args;
+  scm_args.push_back("midi-shell");
+  scm_args.push_back("-q");
+
+  std::string homedir = getenv("HOME");
+  std::string rcfile = homedir + "/.midi-shell-rc";
+  if (access(rcfile.c_str(), F_OK) == 0) {
+    scm_args.push_back("-l");
+    scm_args.push_back(rcfile);
+  }
+
+  scm_init_guile();
+
+  struct ModuleContext {
+    const std::vector<std::string> *scm_args;
+    std::string stdfile;
+  } modcontext;
+
+  std::string lisp_home = std::string(PREFIX) + "/share/midi-shell/lisp";
+
+  modcontext.scm_args = &scm_args;
+  modcontext.stdfile = lisp_home + "/stdlib.scm";
+
+  if (access(modcontext.stdfile.c_str(), F_OK) != 0) {
+      fprintf(stderr, "Could not find lisp home at '%s'\n", lisp_home.c_str());
+      return 1;
+  }
+
+  scm_c_define_module("midi-shell", [](void *userdata) {
+      ModuleContext &modcontext = *(ModuleContext *)userdata;
+      scm_c_define_gsubr("ms:write", 1, 0, 0, (scm_t_subr)ms_write);
+      scm_primitive_load(scm_from_utf8_string(modcontext.stdfile.c_str()));
+    }, &modcontext);
+
+
+  ///
+  RtMidi::Api api = RtMidi::UNSPECIFIED;
+  std::unique_ptr<RtMidiOut> port(new RtMidiOut(api, "MIDI Shell"));
+  ::ms_port = port.get();
+  port->openVirtualPort();
+
+  ///
+  ms_writebuf.reserve(8192);
+
+  ///
+  scm_c_use_module("midi-shell");
+  scm_shell(scm_args.size(),
+            (char **)args_to_argv(scm_args).data());
+
+  return 0;
+}
+
+///
+std::vector<const char *> args_to_argv(const std::vector<std::string> &args)
+{
+  size_t argc = args.size();
+  std::vector<const char *> argv(argc);
+  for (size_t i = 0; i < argc; ++i)
+    argv[i] = args[i].c_str();
+  argv.push_back(nullptr);
+  return argv;
+}
+
+///
+SCM ms_write(SCM msg)
+{
+  RtMidiOut &port = *ms_port;
+  std::vector<uint8_t> &buf = ms_writebuf;
+  size_t len = scm_to_size_t(scm_vector_length(msg));
+
+  try {
+    buf.resize(len);
+  } catch (std::exception &ex) {
+    SCM what = scm_from_utf8_string(ex.what());
+    scm_error(scm_misc_error_key, "ms:write", "~A", scm_list_1(what), SCM_BOOL_F);
+  }
+
+  for (size_t i = 0; i < len; ++i)
+    buf[i] = scm_to_uint8(scm_vector_ref(msg, scm_from_size_t(i)));
+
+  try {
+    port.sendMessage(&buf);
+  } catch (std::exception &ex) {
+    SCM what = scm_from_utf8_string(ex.what());
+    scm_error(scm_misc_error_key, "ms:write", "~A", scm_list_1(what), SCM_BOOL_F);
+  }
+
+  return SCM_BOOL_T;
+}
